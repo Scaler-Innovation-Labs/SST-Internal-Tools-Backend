ec61d1310ed32c18cfab1921e5d06f80d6b3a4c2
de-arnavgupta <arnavgpt28@outlook.com>
2025-04-15 20:39:46 +0530

    repo set up intiated the project and ready to work

---
+++ src/main/java/com/sstinternaltools/sstinternal_tools/SstInternalToolsApplication.java
@@ -0,0 +1,13 @@
+package com.sstinternaltools.sstinternal_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SstInternalToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SstInternalToolsApplication.class, args);
+    }
+
+}

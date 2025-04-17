316d88866f8ecff9896fd98b8b4a7b231d3b0e12
vivek-0509 <viveksolanki0509@gmail.com>
2025-04-18 03:01:35 +0530

    Addded methods to extract any claims that are set into the token

--- src/main/java/com/sstinternaltools/sstinternal_tools/security/service/JwtService.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/security/service/JwtService.java
@@ -6,6 +6,7 @@ import com.sstinternaltools.sstinternal_tools.security.repository.JwtTokenReposi
 import com.sstinternaltools.sstinternal_tools.user.entity.User;
 import com.sstinternaltools.sstinternal_tools.user.entity.UserRole;
 import com.sstinternaltools.sstinternal_tools.user.repository.UserRepository;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -17,6 +18,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public class JwtService {
 
@@ -61,7 +63,11 @@ public class JwtService {
                 .compact();
     }
 
-    public JwtToken generateRefreshToken(String email) {
+    //method to generate refresh token
+    public String generateRefreshToken(String email) {
+        User user=userRepository.findByEmail(email)
+                .orElseThrow(()->new RuntimeException("User with email " + email + " not found."));
+
         String token= Jwts.builder()
                 .claims()
                 .subject(email)
@@ -76,8 +82,33 @@ public class JwtService {
                 TokenType.REFRESH,
                 false,
                 false,
-                LocalDateTime.now()
-        )
+                LocalDateTime.now().plusSeconds(refreshTokenExpiration/1000),
+                user
+        );
+        jwtTokenRepository.save(refreshToken);
+        return token;
+    }
+
+    //method to extract the email stored inside jwt
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims =extractAllClaims(token); //now claim holds all the data inside the Jwt
+        return claimsResolver.apply(claims); //we apply claim resolver function to extract one specific claim
+    }
+
+    //generic method to extract claims from the token
+    private Claims extractAllClaims(String token) {
+        try {
+            return Jwts.parser().verifyWith(getKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (Exception e) {
+            throw new JwtAuthenticationException("Error in extracting claims");
+        }
     }
 
 

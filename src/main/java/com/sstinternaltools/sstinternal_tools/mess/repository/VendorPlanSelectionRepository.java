0df81f4acaed531b0da7734b5d2da677adc38caf
vivek-0509 <viveksolanki0509@gmail.com>
2025-04-23 17:05:50 +0530

    Implemented Vendor plan selection repo

--- src/main/java/com/sstinternaltools/sstinternal_tools/mess/repository/VendorPlanSelectionRepository.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/mess/repository/VendorPlanSelectionRepository.java
@@ -4,6 +4,13 @@ import com.sstinternaltools.sstinternal_tools.mess.entity.VendorPlanSelection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface VendorPlanSelectionRepository extends JpaRepository<VendorPlanSelection, Integer> {
+    List<VendorPlanSelection> findByUserIdAndSelectedMonth(String userId, YearMonth selectedMonth);
+    List<VendorPlanSelection> findByUserId(String userId);
+    List<VendorPlanSelection> findBySelectedMonthAndPlanVendorId(YearMonth selectedMonth, Long vendorId);
 }

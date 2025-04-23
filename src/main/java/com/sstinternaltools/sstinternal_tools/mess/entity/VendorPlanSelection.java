f720e2c368e73646bdfe5d0c483be4996e5d7cfb
vivek-0509 <viveksolanki0509@gmail.com>
2025-04-23 16:13:43 +0530

    Made VendorPlanSelection to map the user and Vendor plan

--- src/main/java/com/sstinternaltools/sstinternal_tools/mess/entity/VendorPlanSelection.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/mess/entity/VendorPlanSelection.java
@@ -1,4 +1,58 @@
 package com.sstinternaltools.sstinternal_tools.mess.entity;
 
+import com.sstinternaltools.sstinternal_tools.user.entity.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class VendorPlanSelection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    private VendorPlan plan;
+
+    private LocalDate month;
+
+    public VendorPlanSelection() {}
+
+    public VendorPlanSelection(User user, LocalDate month, VendorPlan plan) {
+        this.user = user;
+        this.month = month;
+        this.plan = plan;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public VendorPlan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(VendorPlan plan) {
+        this.plan = plan;
+    }
+
+    public LocalDate getMonth() {
+        return month;
+    }
+
+    public void setMonth(LocalDate month) {
+        this.month = month;
+    }
 }

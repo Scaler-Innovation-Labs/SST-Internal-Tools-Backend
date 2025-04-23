451e590b7008286846f4b0247cef43f5722bf57c
vivek-0509 <viveksolanki0509@gmail.com>
2025-04-23 20:03:07 +0530

    Made file structure for all the dtos requires for mess feature

---
+++ src/main/java/com/sstinternaltools/sstinternal_tools/mess/dto/vendorDtos/vendorUpdateDto.java
@@ -0,0 +1,4 @@
+package com.sstinternaltools.sstinternal_tools.mess.dto.vendorDtos;
+
+public class vendorUpdateDto {
+}
---
+++ src/main/java/com/sstinternaltools/sstinternal_tools/mess/dto/vendorPlanSelectionDtos/vendorPlanSelectionSummaryDto.java
@@ -0,0 +1,4 @@
+package com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos;
+
+public class vendorPlanSelectionSummaryDto {
+}

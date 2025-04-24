28f547d2c155fd6c11802e3742612f7e5e10193e
Trinity2206 <srinidhi2206@gmail.com>
2025-04-25 01:20:40 +0530

    feat: created service interfaces and implementation for user vendorplanselection.

---
+++ src/main/java/com/sstinternaltools/sstinternal_tools/mess/service/UserService/Implementation/VendorPlanSelectionUserServiceImpl.java
@@ -0,0 +1,78 @@
+package com.sstinternaltools.sstinternal_tools.mess.service.UserService.Implementation;
+
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionCreateDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionResponseDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionSummaryDto;
+import com.sstinternaltools.sstinternal_tools.mess.entity.VendorPlanSelection;
+import com.sstinternaltools.sstinternal_tools.mess.mapper.implementation.VendorPlanSelectionMapper;
+import com.sstinternaltools.sstinternal_tools.mess.repository.VendorPlanSelectionRepository;
+import com.sstinternaltools.sstinternal_tools.mess.service.UserService.Interface.VendorPlanSelectionUserService;
+import org.springframework.stereotype.Service;
+
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class VendorPlanSelectionUserServiceImpl implements VendorPlanSelectionUserService {
+
+    private final VendorPlanSelectionRepository vendorPlanSelectionRepository;
+    private final VendorPlanSelectionMapper vendorPlanSelectionMapper;
+
+    public VendorPlanSelectionUserServiceImpl(VendorPlanSelectionRepository vendorPlanSelectionRepository, VendorPlanSelectionMapper vendorPlanSelectionMapper) {
+        this.vendorPlanSelectionRepository = vendorPlanSelectionRepository;
+        this.vendorPlanSelectionMapper = vendorPlanSelectionMapper;
+    }
+
+    @Override
+    public VendorPlanSelectionSummaryDto getVendorPlanSelectionById(Long id) {
+        VendorPlanSelection vendorPlanSelection = vendorPlanSelectionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Vendor Plan Selection Not Found"));
+        return vendorPlanSelectionMapper.toSummaryDto(vendorPlanSelection);
+    }
+
+    @Override
+    public List<VendorPlanSelectionSummaryDto> getVendorPlanSelectionsByUserId(Long userId) {
+        List<VendorPlanSelection> vendorPlanSelections = vendorPlanSelectionRepository.findByUserId(userId);
+        List<VendorPlanSelectionSummaryDto> vendorPlanSelectionSummariesDtos = new ArrayList<>();
+        for (VendorPlanSelection vendorPlanSelection : vendorPlanSelections) {
+            vendorPlanSelectionSummariesDtos.add(vendorPlanSelectionMapper.toSummaryDto(vendorPlanSelection));
+        }
+        return vendorPlanSelectionSummariesDtos;
+    }
+
+    @Override
+    public List<VendorPlanSelectionSummaryDto> getVendorPlanSelectionsByMonth(YearMonth month) {
+        List<VendorPlanSelection> vendorPlanSelections = vendorPlanSelectionRepository.findBySelectedMonth(month);
+        List<VendorPlanSelectionSummaryDto> vendorPlanSelectionSummariesDtos = new ArrayList<>();
+        for (VendorPlanSelection vendorPlanSelection : vendorPlanSelections) {
+            vendorPlanSelectionSummariesDtos.add(vendorPlanSelectionMapper.toSummaryDto(vendorPlanSelection));
+        }
+        return vendorPlanSelectionSummariesDtos;
+    }
+
+    @Override
+    public List<VendorPlanSelectionSummaryDto> getVendorPlanSelectionsByMonthAndVendorId(YearMonth month, Long vendorId) {
+        List<VendorPlanSelection> vendorPlanSelections = vendorPlanSelectionRepository.findBySelectedMonthAndPlanVendorId(month, vendorId);
+        List<VendorPlanSelectionSummaryDto> vendorPlanSelectionSummariesDtos = new ArrayList<>();
+        for (VendorPlanSelection vendorPlanSelection : vendorPlanSelections) {
+            vendorPlanSelectionSummariesDtos.add(vendorPlanSelectionMapper.toSummaryDto(vendorPlanSelection));
+        }
+        return vendorPlanSelectionSummariesDtos;
+    }
+
+    @Override
+    public List<VendorPlanSelectionSummaryDto> getVendorPlanSelectionsByUserIdAndMonth(Long userId, YearMonth month) {
+        List<VendorPlanSelection> vendorPlanSelections = vendorPlanSelectionRepository.findByUserIdAndSelectedMonth(userId, month);
+        List<VendorPlanSelectionSummaryDto> vendorPlanSelectionSummariesDtos = new ArrayList<>();
+        for (VendorPlanSelection vendorPlanSelection : vendorPlanSelections) {
+            vendorPlanSelectionSummariesDtos.add(vendorPlanSelectionMapper.toSummaryDto(vendorPlanSelection));
+        }
+        return vendorPlanSelectionSummariesDtos;
+    }
+
+    @Override
+    public VendorPlanSelectionResponseDto createVendorPlanSelection(VendorPlanSelectionCreateDto vendorPlanSelectionCreateDto) {
+        return null;
+    }
+}
---
+++ src/main/java/com/sstinternaltools/sstinternal_tools/mess/service/UserService/Interface/VendorPlanSelectionUserService.java
@@ -0,0 +1,16 @@
+package com.sstinternaltools.sstinternal_tools.mess.service.UserService.Interface;
+
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionCreateDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionResponseDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionSummaryDto;
+import java.time.YearMonth;
+import java.util.List;
+
+public interface VendorPlanSelectionUserService {
+    public VendorPlanSelectionSummaryDto getVendorPlanSelectionById(Long id);
+    public List<VendorPlanSelectionSummaryDto> getVendorPlanSelectionsByUserId(Long userId);
+    public List<VendorPlanSelectionSummaryDto> getVendorPlanSelectionsByMonth(YearMonth month);
+    public List<VendorPlanSelectionSummaryDto> getVendorPlanSelectionsByMonthAndVendorId(YearMonth month, Long vendorId);
+    public List<VendorPlanSelectionSummaryDto> getVendorPlanSelectionsByUserIdAndMonth(Long userId, YearMonth month);
+    public VendorPlanSelectionResponseDto createVendorPlanSelection(VendorPlanSelectionCreateDto vendorPlanSelectionCreateDto);
+}

db230612eddd9c4a91474e605e939dc1b97dff9f
vivek-0509 <viveksolanki0509@gmail.com>
2025-04-25 19:14:33 +0530

    Implemented Bus schedule Summary and update dto

--- src/main/java/com/sstinternaltools/sstinternal_tools/transport/dto/BusScheduleUpdateDto.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/transport/dto/BusScheduleUpdateDto.java
@@ -1,4 +1,55 @@
 package com.sstinternaltools.sstinternal_tools.transport.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class BusScheduleUpdateDto {
+
+    private String source;
+    private String destination;
+    private LocalTime departureTime;
+    private LocalDate date;
+
+    public BusScheduleUpdateDto(String source, String destination, LocalTime departureTime, LocalDate date) {
+        this.source = source;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.date = date;
+    }
+
+    public BusScheduleUpdateDto() {}
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }

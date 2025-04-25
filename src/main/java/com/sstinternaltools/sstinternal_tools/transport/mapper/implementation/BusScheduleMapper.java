a4df0e99e30a10ae0a96dce3bccfdeb3800be069
vivek-0509 <viveksolanki0509@gmail.com>
2025-04-25 19:30:21 +0530

    Imlemented mapper for bus schedule

--- src/main/java/com/sstinternaltools/sstinternal_tools/transport/mapper/implementation/BusScheduleMapper.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/transport/mapper/implementation/BusScheduleMapper.java
@@ -1,4 +1,58 @@
 package com.sstinternaltools.sstinternal_tools.transport.mapper.implementation;
 
-public class BusScheduleMapper {
+import com.sstinternaltools.sstinternal_tools.transport.dto.BusScheduleCreateDto;
+import com.sstinternaltools.sstinternal_tools.transport.dto.BusScheduleResponseDto;
+import com.sstinternaltools.sstinternal_tools.transport.dto.BusScheduleSummaryDto;
+import com.sstinternaltools.sstinternal_tools.transport.dto.BusScheduleUpdateDto;
+import com.sstinternaltools.sstinternal_tools.transport.entity.BusSchedule;
+import com.sstinternaltools.sstinternal_tools.transport.mapper.interfaces.DtoMapper;
+
+public class BusScheduleMapper implements DtoMapper <BusSchedule, BusScheduleResponseDto, BusScheduleCreateDto, BusScheduleUpdateDto, BusScheduleSummaryDto> {
+    @Override
+    public BusSchedule fromCreateDto(BusScheduleCreateDto createDto) {
+        BusSchedule busSchedule = new BusSchedule();
+        busSchedule.setSource(createDto.getSource());
+        busSchedule.setDestination(createDto.getDestination());
+        busSchedule.setDepartureTime(createDto.getDepartureTime());
+        busSchedule.setDate(createDto.getDate());
+        return busSchedule;
+    }
+
+    @Override
+    public BusSchedule fromUpdateDto(BusScheduleUpdateDto updateDto, BusSchedule busSchedule) {
+        if(updateDto.getDepartureTime() != null) {
+            busSchedule.setDepartureTime(updateDto.getDepartureTime());
+        }
+        if(updateDto.getDestination() != null) {
+            busSchedule.setDestination(updateDto.getDestination());
+        }
+        if(updateDto.getDate() != null) {
+            busSchedule.setDate(updateDto.getDate());
+        }
+        if(updateDto.getDepartureTime() != null) {
+            busSchedule.setDepartureTime(updateDto.getDepartureTime());
+        }
+        return busSchedule;
+    }
+
+    @Override
+    public BusScheduleResponseDto toResponseDto(BusSchedule busSchedule) {
+        BusScheduleResponseDto responseDto = new BusScheduleResponseDto();
+        responseDto.setDepartureTime(busSchedule.getDepartureTime());
+        responseDto.setDestination(busSchedule.getDestination());
+        responseDto.setSource(busSchedule.getSource());
+        responseDto.setDate(busSchedule.getDate());
+        responseDto.setId(busSchedule.getId());
+        return responseDto;
+    }
+
+    @Override
+    public BusScheduleSummaryDto toSummaryDto(BusSchedule busSchedule) {
+        BusScheduleSummaryDto summaryDto = new BusScheduleSummaryDto();
+        summaryDto.setDepartureTime(busSchedule.getDepartureTime());
+        summaryDto.setDestination(busSchedule.getDestination());
+        summaryDto.setSource(busSchedule.getSource());
+        summaryDto.setDate(busSchedule.getDate());
+        return summaryDto;
+    }
 }

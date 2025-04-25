a5bcbd68a39b6d5484bf8b18044892e8d93198d8
vivek-0509 <viveksolanki0509@gmail.com>
2025-04-26 00:51:36 +0530

    Made some changes in repo

--- src/main/java/com/sstinternaltools/sstinternal_tools/transport/repository/BusScheduleRepository.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/transport/repository/BusScheduleRepository.java
@@ -2,10 +2,12 @@ package com.sstinternaltools.sstinternal_tools.transport.repository;
 
 import com.sstinternaltools.sstinternal_tools.transport.entity.BusSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface BusScheduleRepository extends JpaRepository<BusSchedule, Long> {
     List<BusSchedule> findAllByDate(LocalDate date);
     boolean existsByDate(LocalDate date);

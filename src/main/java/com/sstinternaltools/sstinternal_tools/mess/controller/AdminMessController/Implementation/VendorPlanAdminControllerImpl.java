59af0655c02e50e0825a306ce47b11225b1fdfad
Trinity2206 <srinidhi2206@gmail.com>
2025-04-29 20:40:52 +0530

    feat: Made vendor plan admin controller and its implementation.

---
+++ src/main/java/com/sstinternaltools/sstinternal_tools/mess/controller/AdminMessController/Implementation/VendorPlanAdminControllerImpl.java
@@ -0,0 +1,68 @@
+package com.sstinternaltools.sstinternal_tools.mess.controller.AdminMessController.Implementation;
+
+import com.sstinternaltools.sstinternal_tools.mess.controller.AdminMessController.Interface.VendorPlanAdminController;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanDtos.VendorPlanCreateDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanDtos.VendorPlanResponseDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanDtos.VendorPlanSummaryDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanDtos.VendorPlanUpdateDto;
+import com.sstinternaltools.sstinternal_tools.mess.service.AdminMessService.Interface.VendorPlanAdminService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/mess/vendorPlan")
+public class VendorPlanAdminControllerImpl implements VendorPlanAdminController {
+
+    private final VendorPlanAdminService vendorPlanAdminService;
+
+    public VendorPlanAdminControllerImpl(VendorPlanAdminService vendorPlanAdminService) {
+        this.vendorPlanAdminService = vendorPlanAdminService;
+    }
+
+    @Override
+    @GetMapping("/fetch/{id}")
+    public ResponseEntity<VendorPlanSummaryDto> getVendorPlanById(@PathVariable Long id) {
+        VendorPlanSummaryDto vendorPlanSummaryDto = vendorPlanAdminService.getVendorPlanById(id);
+        return ResponseEntity.ok(vendorPlanSummaryDto);
+    }
+
+    @Override
+    @GetMapping("/fetchAll")
+    public ResponseEntity<List<VendorPlanSummaryDto>> getAllVendorPlans() {
+        List<VendorPlanSummaryDto> vendorPlanSummaryDtos = vendorPlanAdminService.getAllVendorPlans();
+        return ResponseEntity.ok(vendorPlanSummaryDtos);
+    }
+
+    @Override
+    @PostMapping("/create")
+    public ResponseEntity<VendorPlanResponseDto> createVendorPlan(@Valid @RequestBody VendorPlanCreateDto vendorPlanCreateDto) {
+        VendorPlanResponseDto vendorPlanResponseDto = vendorPlanAdminService.createVendorPlan(vendorPlanCreateDto);
+        return ResponseEntity.ok(vendorPlanResponseDto);
+    }
+
+    @Override
+    @PutMapping("/update/{id}")
+    public ResponseEntity<VendorPlanResponseDto> updateVendorPlan(@Valid @RequestBody VendorPlanUpdateDto vendorPlanUpdateDto, @PathVariable Long id) {
+        VendorPlanResponseDto vendorPlanResponseDto = vendorPlanAdminService.updateVendorPlan(vendorPlanUpdateDto, id);
+        return ResponseEntity.ok(vendorPlanResponseDto);
+    }
+
+    @Override
+    @PatchMapping("/partialUpdate/{id}")
+    public ResponseEntity<VendorPlanResponseDto> partialUpdateVendorPlan(@Valid @RequestBody VendorPlanUpdateDto vendorPlanUpdateDto, @PathVariable Long id) {
+        VendorPlanResponseDto vendorPlanResponseDto = vendorPlanAdminService.partialUpdateVendorPlan(vendorPlanUpdateDto, id);
+        return ResponseEntity.ok(vendorPlanResponseDto);
+    }
+
+    @Override
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteVendorPlan(@PathVariable Long id) {
+        vendorPlanAdminService.deleteVendorPlan(id);
+        return ResponseEntity.ok("Vendor plan deleted successfully");
+    }
+}
--- src/main/java/com/sstinternaltools/sstinternal_tools/mess/controller/AdminMessController/Interface/VendorPlanAdminController.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/mess/controller/AdminMessController/Interface/VendorPlanAdminController.java
@@ -1,4 +1,20 @@
 package com.sstinternaltools.sstinternal_tools.mess.controller.AdminMessController.Interface;
 
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanDtos.VendorPlanCreateDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanDtos.VendorPlanResponseDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanDtos.VendorPlanSummaryDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanDtos.VendorPlanUpdateDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+import java.util.List;
+
 public interface VendorPlanAdminController {
+    public ResponseEntity<VendorPlanSummaryDto> getVendorPlanById(Long id);
+    public ResponseEntity<List<VendorPlanSummaryDto>> getAllVendorPlans();
+    public ResponseEntity<VendorPlanResponseDto> createVendorPlan(VendorPlanCreateDto vendorPlanCreateDto);
+    public ResponseEntity<VendorPlanResponseDto> updateVendorPlan(VendorPlanUpdateDto vendorPlanUpdateDto, Long id);
+    ResponseEntity<VendorPlanResponseDto> partialUpdateVendorPlan(VendorPlanUpdateDto vendorPlanUpdateDto, Long id);
+
+    public ResponseEntity<String> deleteVendorPlan(Long id);
 }

29957c3b683c6d9f592259cdd32656f99b0cc644
Trinity2206 <srinidhi2206@gmail.com>
2025-05-06 22:58:41 +0530

    feat: Added update endpoint and in vendorPlanSelectionAdmin controller interface + implementation.

--- src/main/java/com/sstinternaltools/sstinternal_tools/mess/controller/AdminMessController/Implementation/VendorPlanSelectionAdminControllerImpl.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/mess/controller/AdminMessController/Implementation/VendorPlanSelectionAdminControllerImpl.java
@@ -4,13 +4,14 @@ package com.sstinternaltools.sstinternal_tools.mess.controller.AdminMessControll
 import com.sstinternaltools.sstinternal_tools.mess.controller.AdminMessController.Interface.VendorPlanSelectionAdminController;
 import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionCreateDto;
 import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionResponseDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionUpdateDto;
 import com.sstinternaltools.sstinternal_tools.mess.service.AdminMessService.Interface.VendorPlanSelectionAdminService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/mess/vendorPlanSelection")
+@RequestMapping("/mess/admin/vendorPlanSelection")
 public class VendorPlanSelectionAdminControllerImpl implements VendorPlanSelectionAdminController {
 
     private final VendorPlanSelectionAdminService vendorPlanSelectionAdminService;
@@ -23,6 +24,13 @@ public class VendorPlanSelectionAdminControllerImpl implements VendorPlanSelecti
     @PostMapping("/create")
     public ResponseEntity<VendorPlanSelectionResponseDto> createVendorPlanSelection(@Valid @RequestBody VendorPlanSelectionCreateDto vendorPlanSelectionCreateDto, @Valid @RequestBody Long vendorPlanId, @Valid @RequestBody Long userId) {
         VendorPlanSelectionResponseDto vendorPlanSelectionResponseDto = vendorPlanSelectionAdminService.createVendorPlanSelection(vendorPlanSelectionCreateDto, vendorPlanId, userId);
+        return ResponseEntity.ok(vendorPlanSelectionResponseDto);
+    }
+
+    @Override
+    @PutMapping("/update")
+    public ResponseEntity<VendorPlanSelectionResponseDto> updateVendorPlanSelection(VendorPlanSelectionUpdateDto vendorPlanSelectionUpdateDto, Long vendorPlanSelectionId) {
+        VendorPlanSelectionResponseDto vendorPlanSelectionResponseDto = vendorPlanSelectionUserService.updateVendorPlanSelection(vendorPlanSelectionUpdateDto, vendorPlanSelectionId);
         return ResponseEntity.ok(vendorPlanSelectionResponseDto);
     }
 
--- src/main/java/com/sstinternaltools/sstinternal_tools/mess/controller/AdminMessController/Interface/VendorPlanSelectionAdminController.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/mess/controller/AdminMessController/Interface/VendorPlanSelectionAdminController.java
@@ -2,9 +2,11 @@ package com.sstinternaltools.sstinternal_tools.mess.controller.AdminMessControll
 
 import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionCreateDto;
 import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionResponseDto;
+import com.sstinternaltools.sstinternal_tools.mess.dto.vendorPlanSelectionDtos.VendorPlanSelectionUpdateDto;
 import org.springframework.http.ResponseEntity;
 
 public interface VendorPlanSelectionAdminController {
     public ResponseEntity<VendorPlanSelectionResponseDto> createVendorPlanSelection(VendorPlanSelectionCreateDto vendorPlanSelectionCreateDto, Long vendorPlanId, Long userId);
+    public ResponseEntity<VendorPlanSelectionResponseDto> updateVendorPlanSelection(VendorPlanSelectionUpdateDto vendorPlanSelectionUpdateDto, Long vendorPlanSelectionId);
     public ResponseEntity<String> deleteVendorPlanSelection(Long id);
 }

b0151ed0baa6860ad9907ffccbc70c60574097ba
de-arnavgupta <arnavgpt28@outlook.com>
2025-05-07 05:07:35 +0530

    refactored announcementServiceImpl to reutrn response entity

--- src/main/java/com/sstinternaltools/sstinternal_tools/announcement/service/implementation/AnnouncementServiceImpl.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/announcement/service/implementation/AnnouncementServiceImpl.java
@@ -19,11 +19,10 @@ import org.springframework.data.domain.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+
+import java.util.*;
 
 @Service
 @Transactional

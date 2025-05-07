93343937eb62f5268baf4e1575352c30a8de95ad
Trinity2206 <srinidhi2206@gmail.com>
2025-05-07 05:48:03 +0530

    fix: Allowed auth2/refresh to bypass jwtfilter.

--- src/main/java/com/sstinternaltools/sstinternal_tools/security/config/JwtFilter.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/security/config/JwtFilter.java
@@ -35,6 +35,13 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        String path = request.getRequestURI();
+
+        if (path.startsWith("/auth")) {
+            filterChain.doFilter(request, response); // Skip JWT check
+            return;
+        }
+
         final String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {

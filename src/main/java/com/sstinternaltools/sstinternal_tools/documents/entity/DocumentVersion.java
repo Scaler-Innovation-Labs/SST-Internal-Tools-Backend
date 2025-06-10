6bbd109ef8358e5a3ff9d4d7960fab420f94394e
vivek-0509 <viveksolanki0509@gmail.com>
2025-06-10 23:37:37 +0530

    Implemented the document version entity

--- src/main/java/com/sstinternaltools/sstinternal_tools/documents/entity/DocumentVersion.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/documents/entity/DocumentVersion.java
@@ -2,22 +2,75 @@ package com.sstinternaltools.sstinternal_tools.documents.entity;
 
 import jakarta.persistence.*;
 
-import java.util.Set;
+import java.time.LocalDateTime;
 
 @Entity
 public class DocumentVersion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "document_version_id", nullable = false)
-    private Long versionId;
-    @ManyToOne
-    @JoinColumn(name="document_id")
+    private Long id;
+
+    @ManyToOne(optional = false)
     private Document document;
-    @ElementCollection
-    @Enumerated(EnumType.STRING)
 
-    private Set<AllowedUsers> allowedUsers;
-    @ManyToMany
-    private Set<Tag> tags;
+    private int versionNumber;
 
+    private String fileUrl; // Cloud URL or file path
+
+    private LocalDateTime uploadedAt ;
+
+    private Long uploadedByUserId;
+
+    public DocumentVersion(Document document, int versionNumber, String fileUrl, Long uploadedByUserId) {
+        this.document = document;
+        this.versionNumber = versionNumber;
+        this.fileUrl = fileUrl;
+        this.uploadedByUserId = uploadedByUserId;
+    }
+
+    public DocumentVersion() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    public int getVersionNumber() {
+        return versionNumber;
+    }
+
+    public void setVersionNumber(int versionNumber) {
+        this.versionNumber = versionNumber;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(LocalDateTime uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+
+    public Long getUploadedByUserId() {
+        return uploadedByUserId;
+    }
+
+    public void setUploadedByUserId(Long uploadedByUserId) {
+        this.uploadedByUserId = uploadedByUserId;
+    }
 }

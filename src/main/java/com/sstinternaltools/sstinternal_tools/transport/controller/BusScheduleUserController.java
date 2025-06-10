6e09b8ff1f6d38c8650aa1adc77de657dfdaff83
vivek-0509 <viveksolanki0509@gmail.com>
2025-06-10 05:33:18 +0530

    Made some final changes

--- src/main/java/com/sstinternaltools/sstinternal_tools/transport/controller/BusScheduleUserController.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/transport/controller/BusScheduleUserController.java
@@ -9,7 +9,6 @@ import com.sstinternaltools.sstinternal_tools.transport.entity.BusSchedule;
 import com.sstinternaltools.sstinternal_tools.transport.facade.interfaces.BusScheduleFacade;
 import com.sstinternaltools.sstinternal_tools.transport.mapper.interfaces.BusDtoMapper;
 import jakarta.validation.Valid;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,16 +35,5 @@ public class BusScheduleUserController {
                 .map(event -> dtoMapper.toSummaryDto(event))
                 .collect(Collectors.toList());
         return ResponseEntity.ok(listSchedule);
-    }
-
-    @GetMapping("/getBy/date-range")
-    public ResponseEntity<List<BusScheduleSummaryDto>> searchByDateRange(
-            @RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
-            @RequestParam("end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end) {
-        List<BusScheduleSummaryDto> events= busScheduleFacade.searchBusScheduleByDateRange(start, end)
-                .stream()
-                .map(event -> dtoMapper.toSummaryDto(event))
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(events);
     }
 }

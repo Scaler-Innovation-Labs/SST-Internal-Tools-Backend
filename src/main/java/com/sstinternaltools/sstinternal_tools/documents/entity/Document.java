a7e6efc6f5428e8b7aa37b7cccf728dcb9b89dc5
vivek-0509 <viveksolanki0509@gmail.com>
2025-06-10 23:39:02 +0530

    Made few changes in Document entity

--- src/main/java/com/sstinternaltools/sstinternal_tools/documents/entity/Document.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/documents/entity/Document.java
@@ -25,4 +25,58 @@ public class Document {
 
     @OneToMany(mappedBy = "document", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Tag> tags;
+
+    public Document(String title, String code, DocumentCategory category, Set<AllowedUsers> allowedUsers, Set<Tag> tags) {
+        this.title = title;
+        this.code = code;
+        this.category = category;
+        this.allowedUsers = allowedUsers;
+        this.tags = tags;
+    }
+
+    public Document() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public DocumentCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(DocumentCategory category) {
+        this.category = category;
+    }
+
+    public Set<AllowedUsers> getAllowedUsers() {
+        return allowedUsers;
+    }
+
+    public void setAllowedUsers(Set<AllowedUsers> allowedUsers) {
+        this.allowedUsers = allowedUsers;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
 }

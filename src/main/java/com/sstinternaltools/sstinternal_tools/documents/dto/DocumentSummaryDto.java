1366dc46588b7066d666f521a1c3dbd104f9d388
vivek-0509 <viveksolanki0509@gmail.com>
2025-06-11 10:29:48 +0530

    implemented Document Summary dto

--- src/main/java/com/sstinternaltools/sstinternal_tools/documents/dto/DocumentSummaryDto.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/documents/dto/DocumentSummaryDto.java
@@ -12,4 +12,53 @@ public class DocumentSummaryDto {
     private String latestFilePath;
     private LocalDateTime uploadedAt;
 
+    public DocumentSummaryDto(String title, DocumentCategory category, Set<String> tags, String latestFilePath, LocalDateTime uploadedAt) {
+        this.title = title;
+        this.category = category;
+        this.tags = tags;
+        this.latestFilePath = latestFilePath;
+        this.uploadedAt = uploadedAt;
+    }
+
+    public DocumentSummaryDto() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public DocumentCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(DocumentCategory category) {
+        this.category = category;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getLatestFilePath() {
+        return latestFilePath;
+    }
+
+    public void setLatestFilePath(String latestFilePath) {
+        this.latestFilePath = latestFilePath;
+    }
+
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(LocalDateTime uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
 }

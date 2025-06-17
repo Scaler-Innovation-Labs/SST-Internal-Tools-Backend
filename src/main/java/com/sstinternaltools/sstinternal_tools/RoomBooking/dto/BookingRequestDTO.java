2ed989cdc2ef349ca55bc5344796c963a505de95
Rudray Mehra <rudraymehra@gmail.com>
2025-06-17 22:07:18 +0530

    booking request DTO

---
+++ src/main/java/com/sstinternaltools/sstinternal_tools/RoomBooking/dto/BookingRequestDTO.java
@@ -0,0 +1,54 @@
+package com.sstinternaltools.sstinternal_tools.RoomBooking.dto;
+
+import java.time.LocalDateTime;
+
+public class BookingRequestDTO {
+    private Long roomId;
+    private Long userId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    // Constructors
+    public BookingRequestDTO() {
+    }
+
+    public BookingRequestDTO(Long roomId, Long userId, LocalDateTime startTime, LocalDateTime endTime) {
+        this.roomId = roomId;
+        this.userId = userId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    // Getters and Setters
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+}
--- src/main/java/com/sstinternaltools/sstinternal_tools/RoomBooking/dto/demo.java
+++
@@ -1,4 +0,0 @@
-package com.sstinternaltools.sstinternal_tools.RoomBooking.dto;
-
-public class demo {
-}

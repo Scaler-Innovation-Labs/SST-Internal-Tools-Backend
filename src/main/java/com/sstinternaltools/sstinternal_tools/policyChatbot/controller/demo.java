74ea58d219868caa92cca82833420003484c5421
vivek-0509 <viveksolanki0509@gmail.com>
2025-07-23 08:22:20 +0530

    Made the folder structure for policy chatbot

---
+++ src/main/java/com/sstinternaltools/sstinternal_tools/policyChatbot/controller/demo.java
@@ -0,0 +1,4 @@
+package com.sstinternaltools.sstinternal_tools.policyChatbot.controller;
+
+public class demo {
+}
--- src/main/java/com/sstinternaltools/sstinternal_tools/security/service/implementation/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/security/service/implementation/CustomOAuth2SuccessHandler.java
@@ -72,7 +72,7 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
             response.addHeader("Set-Cookie", refreshCookie.toString());
 
             String redirectUrl = frontendUrl + "/auth/callback";
-//            response.sendRedirect(redirectUrl);
+            response.sendRedirect(redirectUrl);
 
 
 

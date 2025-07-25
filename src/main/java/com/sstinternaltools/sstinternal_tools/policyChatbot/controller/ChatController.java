3d28ba3372b6d40e2754a2684eceecf9046b98c4
vivek-0509 <viveksolanki0509@gmail.com>
2025-07-25 21:53:39 +0530

    Made change in the chat controller

--- src/main/java/com/sstinternaltools/sstinternal_tools/policyChatbot/controller/ChatController.java
+++ src/main/java/com/sstinternaltools/sstinternal_tools/policyChatbot/controller/ChatController.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/policyChatBot")
 public class ChatController {
 
     private final ChatService chatService;
